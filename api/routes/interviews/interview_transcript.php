c2b3c9774a454f7cafe25c9b9d3b2559f66ef8db
Ethan <e.thom.butler@gmail.com>
2017-05-02 11:01:50 -0400

    fix return value for interviews with no transcript provided

--- api/routes/interviews/interview_transcript.php
+++ api/routes/interviews/interview_transcript.php
@@ -2,6 +2,7 @@
 $route = new Route('/interviews/(?P<id>\d+)/transcript', 'GET', function($data){
   $transcript = new Transcript($data['id']);
   $sliced = $transcript->get_slices_and_breaks();
+  if(!$sliced) return [];
   foreach($sliced as &$slice){
     if($slice['start']) $slice['start'] = sanitize_timestamp($slice['start']);
     if($slice['end'])   $slice['end']   = sanitize_timestamp($slice['end']);
