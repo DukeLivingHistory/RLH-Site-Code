460a9b1e5b2e1518aee4944bf63da0c243f5d553
ethanbutler <e.thom.butler@gmail.com>
2018-02-28 09:31:22 -0500

    fix text change for view more/less, #49

--- app/src/js/cutoff.js
+++ app/src/js/cutoff.js
@@ -6,7 +6,7 @@ const cutoff = (elem) => {
     const $this = $(this)
     const $target = $this.siblings($this.data('cutoff'))
     $this.data('on', !$this.data('on'))
-    $this.data('orig', $this.data('orig') || $this.text())
+    $this.data('orig', $this.data('orig') || $this.html())
 
     if($this.data('on')) {
       $target.show()
