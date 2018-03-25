752125933ed8157ce9b561761a167d1da00fac2b
ethanbutler <e.thom.butler@gmail.com>
2018-03-25 14:48:44 -0400

    remove gulp deps, move to npm scripts

--- lib/get_app_part.php
+++ lib/get_app_part.php
@@ -7,8 +7,8 @@ function get_app() {
   $dir = get_stylesheet_directory_uri();
   ?>
   <div class="app-wrapper"></div>
-  <script src="<?= $dir; ?>/assets/dist/js/ableplayer.min.js"></script>
-  <script src="<?= $dir; ?>/assets/dist/js/able-timestamps.min.js"></script>
-  <script src="<?= $dir; ?>/assets/dist/js/able-search.min.js"></script>
+  <script src="<?= $dir; ?>/assets/vendor/js/ableplayer.min.js"></script>
+  <script src="<?= $dir; ?>/assets/vendor/js/able-timestamps.min.js"></script>
+  <script src="<?= $dir; ?>/assets/vendor/js/able-search.min.js"></script>
   <?php
 }
