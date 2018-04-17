09380c91463756da963aedde1d8dd866079c0a46
ethanbutler <e.thom.butler@gmail.com>
2018-04-17 19:08:20 -0400

    missing featherlight module

--- lib/assets.php
+++ lib/assets.php
@@ -7,7 +7,7 @@
 add_action( 'wp_enqueue_scripts', function(){
   wp_dequeue_script( 'jquery' );
   wp_enqueue_script( 'jquery-cdn', 'https://ajax.googleapis.com/ajax/libs/jquery/2.2.2/jquery.min.js', null, null, false );
-  wp_enqueue_script( 'featherlight-js', get_stylesheet_directory_uri().'/app/src/js/thirdparty/featherlight.min.js', null, null, true );
+  wp_enqueue_script( 'featherlight-js', get_stylesheet_directory_uri().'/assets/vendor/js/featherlight.min.js', null, null, true );
   wp_enqueue_script( 'main-js', get_stylesheet_directory_uri().'/assets/dist/js/bundle.js?update=5', null, null, true );
   wp_enqueue_style( 'main-css', get_stylesheet_directory_uri().'/assets/dist/css/styles.min.css?update=5', false, null );
 } );
