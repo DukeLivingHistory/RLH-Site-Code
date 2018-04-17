888584dde83af7f5ca9108fcd29f8d83f809551d
ethanbutler <e.thom.butler@gmail.com>
2018-04-17 19:45:08 -0400

    fix duped jquery bug on production

--- lib/assets.php
+++ lib/assets.php
@@ -5,6 +5,7 @@
  */
 
 add_action( 'wp_enqueue_scripts', function(){
+  wp_deregister_script('jquery');
   wp_dequeue_script( 'jquery' );
   wp_enqueue_script( 'jquery-cdn', 'https://ajax.googleapis.com/ajax/libs/jquery/2.2.2/jquery.min.js', null, null, false );
   wp_enqueue_script( 'featherlight-js', get_stylesheet_directory_uri().'/assets/vendor/js/featherlight.min.js', null, null, true );
