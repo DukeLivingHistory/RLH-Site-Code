0c9b464086a532e553e3ac3a93f4863e3b826cdf
ethanbutler <e.thom.butler@gmail.com>
2018-12-21 16:42:27 -0500

    cachebust

--- lib/assets.php
+++ lib/assets.php
@@ -9,8 +9,8 @@ add_action( 'wp_enqueue_scripts', function(){
   wp_dequeue_script( 'jquery' );
   wp_enqueue_script( 'jquery-cdn', 'https://ajax.googleapis.com/ajax/libs/jquery/2.2.2/jquery.min.js', null, null, false );
   wp_enqueue_script( 'featherlight-js', get_template_directory_uri().'/assets/vendor/js/featherlight.min.js', null, null, true );
-  wp_enqueue_script( 'main-js', get_template_directory_uri().'/assets/dist/js/bundle.js?update=5', null, null, true );
-  wp_enqueue_style( 'main-css', get_template_directory_uri().'/assets/dist/css/styles.min.css?update=5', false, null );
+  wp_enqueue_script( 'main-js', get_template_directory_uri().'/assets/dist/js/bundle.js?update=6', null, null, true );
+  wp_enqueue_style( 'main-css', get_template_directory_uri().'/assets/dist/css/styles.min.css?update=6', false, null );
 
   $terms = get_terms([
     'taxonomy' => 'nav_menu'
